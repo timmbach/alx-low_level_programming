84c47fb9e2c67a2d82e6b947387247c491b8951f
Timothy Mbachu <follow.timothy@gmail.com>
2023-08-10 21:47:14 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
 int main(void)
 {
 const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-fprintf(stderr, %s, message);
+fprintf(stderr, "%s", message);
 return (1);
 } 
