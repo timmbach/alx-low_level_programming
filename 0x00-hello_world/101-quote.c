bf497aea9f18cafc7b24327d0b3407b7bb38cf40
Timothy Mbachu <follow.timothy@gmail.com>
2023-08-10 21:45:16 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
  * main - entry point
  * Return: 1 (error)
  */
-int main(void)
+nt main(void)
 {
 const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 fprintf(stderr, %s, message);
