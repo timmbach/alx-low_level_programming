44278bd5e0a9ee83b0262d12c5861c5292431917
Timothy Mbachu <follow.timothy@gmail.com>
2023-08-11 19:32:24 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char letter = 'a';
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(letter);
+putchar(toupper(letter));
+letter++;
+}
+putchar('\n');
+return (0);
+}
