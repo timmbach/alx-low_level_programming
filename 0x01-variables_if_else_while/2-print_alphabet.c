fe5282eec4ae526da881850ebe72cb33a4b650e7
Timothy Mbachu <follow.timothy@gmail.com>
2023-08-11 19:10:03 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabets.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+char letter = 'a';
+int i;
+for (i = 0; i< 26; i++)
+{
+putchar(letter);
+letter++;
+}
+putchar('/n');
+return (0);
+}
