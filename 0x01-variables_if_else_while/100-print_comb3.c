3ff15d2453f5601bf3bea0a22c51a530515bff79
Timothy Mbachu <follow.timothy@gmail.com>
2023-08-11 21:38:52 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers between 00 to 99.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int i, e;
+i = 48;
+e = 48;
+while (i < 58)
+{
+while (e < 58)
+{
+putchar(i);
+putchar(e);
+if (i == e)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+i++;
+}
+e++;
+}
+putchar('\n');
+return (0);
+}
