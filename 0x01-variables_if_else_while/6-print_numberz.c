412c98d3f1374f8f3d62b94d0e3046d619c6ffce
Timothy Mbachu <follow.timothy@gmail.com>
2023-08-11 20:14:50 -0700

    V

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers between 0 and 9.
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
