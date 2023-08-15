c6e215c18c2afcb23d0f7e1536e0b75dda122116
Timothy Mbachu <follow.timothy@gmail.com>
2023-08-15 23:46:02 +0100

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times
+ *
+ * Return: Always returns 0
+ */
+void print_alphabet_x10(void)
+{
+int i;
+int j;
+for (i = 0; i < 10; i++)
+{
+for (j = 97; j < 123; j++)
+{
+_putchar(j);
+}
+_putchar('\n');
+}
+}
+     
