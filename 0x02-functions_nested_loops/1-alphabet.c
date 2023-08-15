35af400d00ee82e5f65242810eb270d4b6968ae2
Timothy Mbachu <follow.timothy@gmail.com>
2023-08-15 23:36:19 +0100

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,9 +8,9 @@
 void print_alphabet(void)
 {
 int i;
- for (i = 97; i < 123; i++)
-   {
-     _putchar(i);
-   }
- _putchar('\n');
+for (i = 97; i < 123; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
