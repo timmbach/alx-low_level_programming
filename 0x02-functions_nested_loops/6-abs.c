55da3d3dae3aabdf8d143091ebf434cc5f91ccee
Timothy Mbachu <follow.timothy@gmail.com>
2023-08-16 01:01:54 +0100

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - computes absolute value
+ *
+ * @i: input number as integer
+ *
+ * Return: Always 0
+ */
+int _abs(int i)
+{
+if (i < 0)
+{
+return (i * -1);
+}
+else
+{
+return (i);
+}
+}
