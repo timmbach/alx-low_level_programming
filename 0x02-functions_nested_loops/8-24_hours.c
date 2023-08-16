bc5be2b157c43ad6a07cc1c9ecd675fca8ccb380
Timothy Mbachu <follow.timothy@gmail.com>
2023-08-16 02:10:18 +0100

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,17 +6,15 @@
  */
 void jack_bauer(void)
 {
-int a,b,c,d;
-for (a = 48; a <= 50; a++)
+int a, b, c, d;
+for (a = 48; a < 50; a++)
 {
-for (b = 48; b <= 51; b++)
+for (b = 48; b < 51; b++)
 {
-for (c  = 48; c <= 53; c++)
+for (c  = 48; c < 53; c++)
 {
-for (d = 48; d <= 57; d++)
+for (d = 48; d < 57; d++)
 {
- if(b > 51 || c > 53)
- break;
 _putchar(a);
 _putchar(b);
 _putchar(58);
