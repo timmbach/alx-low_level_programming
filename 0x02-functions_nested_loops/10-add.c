66bda33b8f2a019bcc22258639e5831e926b2cab
Timothy Mbachu <follow.timothy@gmail.com>
2023-08-16 02:36:10 +0100

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds to integers and returns result
+ *
+ * @a: input as integer
+ * @b: input as integer
+ * Return: result of a + b
+ */
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+}
