80aebfaf60cf7cb86d194bcf0bb90d244ae2778f
Timothy Mbachu <follow.timothy@gmail.com>
2023-08-22 02:45:32 +0100

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _strlen - returns length of string
+ * @s: input string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+int length = sizeof(*s)/sizeof(*s[0]);
+return length;
+}
